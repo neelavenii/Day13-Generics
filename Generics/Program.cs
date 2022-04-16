ae4860dcd188174a215633b9490fa5db5b92a7cb
eswar <neelaveni.eswar04@gmail.com>
2022-04-16 07:35:09 +0530

    [Eswar] Welcome to Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Generics");
+            
+            Console.ReadLine();
+
+        }
+    }
+}
