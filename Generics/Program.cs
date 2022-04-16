678eaf42a908fbba6d226e79767410ff2fa133e4
eswar <neelaveni.eswar04@gmail.com>
2022-04-16 09:15:38 +0530

    [Eswar] Print Array elements without Generics

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -11,19 +11,39 @@ namespace Generics
         
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Generics");
-            Console.WriteLine("Enter size of array");
-            int size = Convert.ToInt32(Console.ReadLine());
-            int[] intArr = new int[size];
-            for(int i = 0; i < size; i++)
-            {
-                intArr[i] = Convert.ToInt32(Console.ReadLine());
-            }
+            
+            int[] intArr = { 10, 56, 23 };
             double[] doubleArr = { 10.23, 56.34, 67.23 };
             char[] charArr = { 'H', 'E', 'L', 'L', 'O' };
-           Uc1PrintArray.PrintArrayElements(intArr);
-            Uc1PrintArray.PrintArrayElements(doubleArr);
-            Uc1PrintArray.PrintArrayElements(charArr);
+            Console.WriteLine("Please choose program number to Execute");
+            Console.WriteLine("1:Print Array elements without Generics\n2:Print Array elements without Generics\n");
+                int input = Convert.ToInt32(Console.ReadLine());
+            
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Enter size of array");
+                    int size = Convert.ToInt32(Console.ReadLine());
+                     intArr = new int[size];
+                    for (int i = 0; i < size; i++)
+                    {
+                        intArr[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    
+                    Uc1PrintArray.PrintArrayElements(intArr);
+                    Uc1PrintArray.PrintArrayElements(doubleArr);
+                    Uc1PrintArray.PrintArrayElements(charArr);
+                    break;
+                    case 2:
+                    Uc2GenericPrintArray.PrintArrayElements<int>(intArr);
+                    Uc2GenericPrintArray.PrintArrayElements<double>(doubleArr);
+                    Uc2GenericPrintArray.PrintArrayElements<char>(charArr);
+                    break;
+                    default:
+                    break;
+            }
+            //Console.WriteLine("Welcome To Generics");
+           
             Console.ReadLine();
 
         }
---
+++ Generics/Uc2GenericPrintArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Uc2GenericPrintArray
+    {
+        public static void PrintArrayElements<T>(T[] arr)
+        {
+            Console.WriteLine("\nArray Elements");
+            foreach (var element in arr)
+            {
+                Console.Write(element+" ");
+            }
+
+        }
+    }
+}
