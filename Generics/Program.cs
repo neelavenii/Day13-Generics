5c4be291637bdf76277333c14d3b6686a81a0081
eswar <neelaveni.eswar04@gmail.com>
2022-04-16 08:33:38 +0530

    [Eswar] Added Print Array Program

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -8,10 +8,22 @@ namespace Generics
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Generics");
-            
+            Console.WriteLine("Enter size of array");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] intArr = new int[size];
+            for(int i = 0; i < size; i++)
+            {
+                intArr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            double[] doubleArr = { 10.23, 56.34, 67.23 };
+            char[] charArr = { 'H', 'E', 'L', 'L', 'O' };
+           Uc1PrintArray.PrintArrayElements(intArr);
+            Uc1PrintArray.PrintArrayElements(doubleArr);
+            Uc1PrintArray.PrintArrayElements(charArr);
             Console.ReadLine();
 
         }
---
+++ Generics/Uc1PrintArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Uc1PrintArray
+    {
+        public static void PrintArrayElements(int[] arr)
+        {
+            foreach (int element in arr)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+        public static void PrintArrayElements(double[] arr)
+        {
+          
+
+        }
+        public static void PrintArrayElements(char[] arr)
+        {
+            foreach (char element in arr)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
